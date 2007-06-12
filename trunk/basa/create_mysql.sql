64b40647d9cdf753ed4a1c677ec1d6beb295667a
vsatayamas <vsatayamas@e7cb7bc6-4f32-0410-933f-db000b6b029f>
2007-06-12 12:55:48 +0000

    update create mysql table script

--- trunk/basa/create_mysql.sql
+++ trunk/basa/create_mysql.sql
@@ -1,41 +1,30 @@
--- MySQL dump 10.11
---
--- Host: 127.0.0.1    Database: zooid
--- ------------------------------------------------------
--- Server version	5.0.37
+# CocoaMySQL dump
+# Version 0.7b5
+# http://cocoamysql.sourceforge.net
+#
+# Host: 127.0.0.1 (MySQL 5.0.37)
+# Database: zooid
+# Generation Time: 2007-06-12 14:53:07 +0200
+# ************************************************************
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+# Dump of table zooid_articles
+# ------------------------------------------------------------
 
---
--- Table structure for table `zooid_articles`
---
-
-DROP TABLE IF EXISTS `zooid_articles`;
 CREATE TABLE `zooid_articles` (
   `id` int(11) NOT NULL auto_increment,
   `title` varchar(255) NOT NULL,
   `body` longtext,
   `progressive` tinyint(1) NOT NULL default '0',
   `need_checking` tinyint(1) NOT NULL default '0',
-  `reserved` tinyint(1) NOT NULL default '0',
   `deleted` tinyint(4) default '0',
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `zooid_comments`
---
 
-DROP TABLE IF EXISTS `zooid_comments`;
+
+# Dump of table zooid_comments
+# ------------------------------------------------------------
+
 CREATE TABLE `zooid_comments` (
   `id` int(11) NOT NULL auto_increment,
   `article_id` int(11) NOT NULL,
@@ -43,24 +32,37 @@ CREATE TABLE `zooid_comments` (
   `comment` longtext NOT NULL,
   `timestamp` int(11) NOT NULL default '0',
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `zooid_groups`
---
 
-DROP TABLE IF EXISTS `zooid_groups`;
+
+# Dump of table zooid_groups
+# ------------------------------------------------------------
+
 CREATE TABLE `zooid_groups` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(128) default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `zooid_translations`
---
 
-DROP TABLE IF EXISTS `zooid_translations`;
+
+# Dump of table zooid_reservations
+# ------------------------------------------------------------
+
+CREATE TABLE `zooid_reservations` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_id` int(11) NOT NULL,
+  `article_id` int(11) NOT NULL,
+  `expected_submission_date` date default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table zooid_translations
+# ------------------------------------------------------------
+
 CREATE TABLE `zooid_translations` (
   `id` int(11) NOT NULL auto_increment,
   `body` longtext,
@@ -69,13 +71,13 @@ CREATE TABLE `zooid_translations` (
   `title` varchar(255) NOT NULL,
   `user_id` int(11) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=77 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=78 DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `zooid_users`
---
 
-DROP TABLE IF EXISTS `zooid_users`;
+
+# Dump of table zooid_users
+# ------------------------------------------------------------
+
 CREATE TABLE `zooid_users` (
   `id` int(11) NOT NULL auto_increment,
   `username` varchar(255) NOT NULL,
@@ -85,15 +87,7 @@ CREATE TABLE `zooid_users` (
   `group_id` int(11) NOT NULL,
   `email` varchar(255) default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2007-05-28  9:05:04
+
