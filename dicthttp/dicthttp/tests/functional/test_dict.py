8623fc6fc2548f8eb88f78160531cb0822d893a1
vsatayamas <vsatayamas@e7cb7bc6-4f32-0410-933f-db000b6b029f>
2008-12-03 10:34:13 +0000

    remove dicthttp

--- dicthttp/dicthttp/tests/functional/test_dict.py
+++
@@ -1,7 +0,0 @@
-from dicthttp.tests import *
-
-class TestDictController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='dict', action='index'))
-        # Test response...
