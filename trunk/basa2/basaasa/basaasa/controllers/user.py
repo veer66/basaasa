db195020eb470cc8dbc80cd5646916e48d8816b3
vsatayamas <vsatayamas@e7cb7bc6-4f32-0410-933f-db000b6b029f>
2008-12-06 18:56:47 +0000

    user + doc partially implemented

--- trunk/basa2/basaasa/basaasa/controllers/user.py
+++ trunk/basa2/basaasa/basaasa/controllers/user.py
@@ -9,9 +9,23 @@ from authkit.permissions import ValidAuthKitUser
 from authkit.authorize.pylons_adaptors import authorize
 from webhelpers import paginate  
 
+from pylons.decorators import validate
+from pylons.decorators.rest import restrict
+
+import formencode
+from formencode import htmlfill
+
+
 from basaasa import model
 
 log = logging.getLogger(__name__)
+
+class NewUserForm(formencode.Schema):
+    allow_extra_fields = True
+    filter_extra_fields = True
+    username = formencode.validators.String(not_empty=True)
+    password = formencode.validators.String(not_empty=True)
+    group_uid = formencode.validators.Int()
 
 class UserController(BaseController):
 
@@ -26,3 +40,59 @@ class UserController(BaseController):
         users = model.User.query.all()
         c.paginator = paginate.Page(users, page = page)  
         return render("/derived/user/list.html")
+    
+    def new(self):
+        c.groups = [(g.uid, g.name) for g in model.Group.query.all()]
+#        c.groups = ["foo", "titi"]
+        return render("/derived/user/new.html")
+
+    @restrict('POST')
+    @validate(schema=NewUserForm(), form='new')    
+    def create(self):
+        user = model.User(self.form_result['username'])
+        for k, v in self.form_result.items():
+            if k != 'username':
+                setattr(user, k, v)        
+        model.meta.Session.flush()
+        redirect_to(action="list")
+        
+    def view(self, id=None):
+        if id is None:
+            abort(404)
+        c.user = model.User.get(id)        
+        return render("/derived/user/view.html")
+    
+    def edit(self, id=None):
+        if id is None:
+            abort(404)
+        user = model.User.get(id)
+        if user is None:
+            abort(404)
+        values = {"username": user.username}
+        c.groups = model.Group.query.all()
+        return htmlfill.render(render("/derived/user/edit.html"), values)
+    
+    @restrict('POST')
+    @validate(schema=NewUserForm(), form='edit')
+    def save(self, id=None):
+        if id is None:
+            abort(404)
+        user = model.User.get(id)
+        if user is None:
+            abort(404)
+        for k, v in self.form_result.items():
+            if getattr(user, k) != v:
+                setattr(user, k, v)
+        model.meta.Session.flush()
+        redirect_to(action="view", id=id)
+        
+    def delete(self, id=None):
+        if id is None:
+            abort(404)
+        user = model.User.get(id)
+        if user is None:
+            abort(404)
+        c.user = user
+        model.meta.Session.delete(user)
+        model.meta.Session.flush()
+        return render('/derived/user/deleted.html')
