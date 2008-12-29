b1eda33012444d77d59a92ef1710391941703855
andromeda.kuk <andromeda.kuk@e7cb7bc6-4f32-0410-933f-db000b6b029f>
2008-12-29 12:49:06 +0000

    dictionary

--- basa2/basaasa/basaasa/public/js/basaasa.js
+++ basa2/basaasa/basaasa/public/js/basaasa.js
@@ -1,14 +1,17 @@
+
 $(document).ready(function() {
-    
-	$("#input").keydown(update).keyup(update).mousedown(update).mouseup(update).mousemove(update)
+
+	$("#source_body").keydown(update).keyup(update).mousedown(update).mouseup(update).mousemove(update)
+	//$("#body").keydown(update).keyup(update).mousedown(update).mouseup(update).mousemove(update)
 	var old = "";
 	function update() {
 		
-		var t = $("#input").getSelection();
+		var t = $("#source_body").getSelection();
+		//var t = $("#body").getSelection();
 		var url = "http://127.0.0.1/dict/d:" + t.text;
-
+	
 		var callback = function(d) {
-            $("#debug").text("arrive");
+            //$("#debug").text("arrive");
                 all = "";
                 
               for(i = 0; i < d.length; i++) {
@@ -18,13 +21,114 @@ $(document).ready(function() {
                   
                 }
             
-            $("#output").val(all);
+            $("#dictionary").val(all);
 		}
     
         if(t.text != "" && t.text != old) { 
-            $("#debug").text("start");
+            //$("#debug").text("start");
+            $.get(url , {}, callback, "json");
+            old = t.text;
+        }
+	}
+}); 
+
+
+$(document).ready(function() {
+
+	//$("#source_body").keydown(update).keyup(update).mousedown(update).mouseup(update).mousemove(update)
+	$("#body").keydown(update).keyup(update).mousedown(update).mouseup(update).mousemove(update)
+	var old = "";
+	function update() {
+		
+		//var t = $("#source_body").getSelection();
+		var t = $("#body").getSelection();
+		var url = "http://127.0.0.1/dict/d:" + t.text;
+	
+		var callback = function(d) {
+            //$("#debug").text("arrive");
+                all = "";
+                
+              for(i = 0; i < d.length; i++) {
+                      
+                    all = all + "\n" +"Dictionary: " +d[i].database + "\n" + d[i].def+ "\n"+ "\n" ;
+
+                  
+                }
+            
+            $("#dictionary").val(all);
+		}
+    
+        if(t.text != "" && t.text != old) { 
+            //$("#debug").text("start");
             $.get(url , {}, callback, "json");
             old = t.text;
         }
 	}
 });
+
+$(document).ready(function() {
+
+	$("#source_title").keydown(update).keyup(update).mousedown(update).mouseup(update).mousemove(update)
+	//$("#body").keydown(update).keyup(update).mousedown(update).mouseup(update).mousemove(update)
+	var old = "";
+	function update() {
+		
+		var t = $("#source_title").getSelection();
+		//var t = $("#body").getSelection();
+		var url = "http://127.0.0.1/dict/d:" + t.text;
+	
+		var callback = function(d) {
+            //$("#debug").text("arrive");
+                all = "";
+                
+              for(i = 0; i < d.length; i++) {
+                      
+                    all = all + "\n" +"Dictionary: " +d[i].database + "\n" + d[i].def+ "\n"+ "\n" ;
+
+                  
+                }
+            
+            $("#dictionary").val(all);
+		}
+    
+        if(t.text != "" && t.text != old) { 
+            //$("#debug").text("start");
+            $.get(url , {}, callback, "json");
+            old = t.text;
+        }
+	}
+}); 
+
+$(document).ready(function() {
+
+	$("#title").keydown(update).keyup(update).mousedown(update).mouseup(update).mousemove(update)
+	//$("#body").keydown(update).keyup(update).mousedown(update).mouseup(update).mousemove(update)
+	var old = "";
+	function update() {
+		
+		var t = $("#title").getSelection();
+		//var t = $("#body").getSelection();
+		var url = "http://127.0.0.1/dict/d:" + t.text;
+	
+		var callback = function(d) {
+            //$("#debug").text("arrive");
+                all = "";
+                
+              for(i = 0; i < d.length; i++) {
+                      
+                    all = all + "\n" +"Dictionary: " +d[i].database + "\n" + d[i].def+ "\n"+ "\n" ;
+
+                  
+                }
+            
+            $("#dictionary").val(all);
+		}
+    
+        if(t.text != "" && t.text != old) { 
+            //$("#debug").text("start");
+            $.get(url , {}, callback, "json");
+            old = t.text;
+        }
+	}
+}); 
+
