3488b46acb1166fb498cc4223ecdb268a5d4386c
andromeda.kuk <andromeda.kuk@e7cb7bc6-4f32-0410-933f-db000b6b029f>
2009-01-31 22:30:36 +0000

    BasaAsa

--- basa2/basaasa/basaasa/public/js/mt.js
+++ basa2/basaasa/basaasa/public/js/mt.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
 		var myArray=t.split("\n\n");	
 		var url = "http://127.0.0.1/mt";
 		var all = [];
+		var test ="";
+		
 		for(i = 0; i < myArray.length; i++) {
 			var params = {};
 			params['source'] = myArray[i];
@@ -21,10 +23,13 @@ $(document).ready(function() {
 				 "success": function(data) {
 				
 			$("#body").val($("#body").val()+ myArray[i]+"\n"+data+"\n\n");
-							}
-			};			
-			$.ajax(s);	
-		}
+							}			
+			};	
+			
+			test = $.ajax(s);
+			
+					}
+		alert(test);
 }); 
 
 
