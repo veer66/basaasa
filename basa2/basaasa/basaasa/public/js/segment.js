84715fbea9861f74c20e87829768dd4a20f38d03
vsatayamas <vsatayamas@e7cb7bc6-4f32-0410-933f-db000b6b029f>
2009-02-10 17:08:27 +0000

    adjust ajax script for sentence segmentation

--- basa2/basaasa/basaasa/public/js/segment.js
+++ basa2/basaasa/basaasa/public/js/segment.js
@@ -3,14 +3,9 @@ $(document).ready(function() {
 	var url = segment_service_url;
 	var params = {};
 	params['text'] = t;
-	params['lang'] = 'lang';
-	var callback = function(d) {
-		all = "";
-	    for(i = 0; i < d.length-1; i++) {
-	    	all = all+d[i]+"\n\n";
-	    }
-	    all = all.substring(0,all.length-2);
-	    $("#segment").val(all);
+	params['lang'] = 'eng';
+	var callback = function(sentences) {
+	    $("#segment").val(sentences.join("\n\n"));
 	}
     $.post(url ,params, callback, "json");
 }); 
