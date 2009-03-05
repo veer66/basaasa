f4857a3c6281d5527356f696e1e28157e4775133
vsatayamas <vsatayamas@e7cb7bc6-4f32-0410-933f-db000b6b029f>
2009-03-05 13:28:45 +0000

    mysqlstore create table

--- trunk/basa2/basaasa/basaasa/websetup.py
+++ trunk/basa2/basaasa/basaasa/websetup.py
@@ -4,6 +4,8 @@ import logging
 from basaasa.config.environment import load_environment
 from basaasa.users.authkit_elixir_driver import UsersFromDatabase
 from basaasa import model
+from authkit.authenticate.open_id import make_store
+from pylons import config
 
 log = logging.getLogger(__name__)
 
@@ -12,4 +14,9 @@ def setup_app(command, conf, vars):
     load_environment(conf.global_conf, conf.local_conf)
     from elixir import metadata    
     metadata.create_all(checkfirst=True)
+    store_type = config.get('authkit.openid.store.type')
+    stote_config = config.get('authkit.openid.store.config')
+    conn, cstore = make_store(store_type, stote_config)
+    if store_type == 'mysql':
+        cstore.createTables()
 
