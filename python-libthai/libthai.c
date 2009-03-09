b1ccc683bea69ed6ec198ddd582f94ed1f48cd50
vsatayamas <vsatayamas@e7cb7bc6-4f32-0410-933f-db000b6b029f>
2009-03-09 16:57:10 +0000

    handle error and fix mem leak

--- python-libthai/libthai.c
+++ python-libthai/libthai.c
@@ -6,44 +6,56 @@ static PyObject*
 th_brk_(PyObject *self, PyObject *args) 
 {
     PyObject *result = NULL;
-
     Py_UNICODE *s1;
     int s1_len;
 
-    if (PyArg_ParseTuple(args, "u#", &s1, &s1_len)) {
-        if(s1_len > 0) {
-            PyObject *txt_cp874 = PyUnicode_Encode(s1, s1_len, "CP874", NULL);
-            if(txt_cp874 != NULL) {
-                Py_ssize_t len = PyString_Size(txt_cp874);
-                char *c_txt_cp874 = PyString_AsString(txt_cp874);
-                int *pos = (int *)malloc(sizeof(int) * (s1_len + 1)); 
-                int n = th_brk((unsigned char *)c_txt_cp874, pos, len);
-                int i, s = 0;
-                char *buffer;
-                result = PyList_New(0);
-                for(i = 0; i < n; i++) {
-                    PyObject *tok_cp874 = PySequence_GetSlice(txt_cp874, s, pos[i]);
-                    Py_ssize_t tok_len;
-                    PyObject *tok;
-                    PyString_AsStringAndSize(tok_cp874, &buffer, &tok_len);
-                    tok = PyUnicode_Decode(buffer, tok_len, "CP874", NULL);
-                    s = pos[i];
-                    PyList_Append(result, tok);
-                } 
-                if(s < len) {
-                    PyObject *tok_cp874 = PySequence_GetSlice(txt_cp874, s, len);
-                    Py_ssize_t tok_len;
-                    PyObject *tok;
-                    PyString_AsStringAndSize(tok_cp874, &buffer, &tok_len);
-                    tok = PyUnicode_Decode(buffer, tok_len, "CP874", NULL);
-                    PyList_Append(result, tok);
-                }
-                free(pos);
-                return result;
-            }
-        }
+    if (!PyArg_ParseTuple(args, "u#", &s1, &s1_len)) {
+        PyErr_SetString(PyExc_TypeError, "parameter must be unicode");
+        return NULL;
     }
-    return NULL;
+
+    if(s1_len == 0) {
+        PyErr_SetString(PyExc_ValueError, "parameter must not be empty string");
+        return NULL;
+    }
+
+    PyObject *txt_cp874 = PyUnicode_Encode(s1, s1_len, "CP874", NULL);
+    if(txt_cp874 == NULL) {
+        return NULL;
+    }
+
+    Py_ssize_t len = PyString_Size(txt_cp874);
+    char *c_txt_cp874 = PyString_AsString(txt_cp874);
+    int *pos = (int *)malloc(sizeof(int) * (s1_len + 1)); 
+    int n = th_brk((unsigned char *)c_txt_cp874, pos, len);
+    int i, s = 0;
+    char *buffer;
+    result = PyList_New(0);
+    for(i = 0; i < n; i++) {
+        PyObject *tok;
+        PyObject *tok_cp874 = PySequence_GetSlice(txt_cp874, s, pos[i]);
+        Py_ssize_t tok_len;
+        PyString_AsStringAndSize(tok_cp874, &buffer, &tok_len);
+        tok = PyUnicode_Decode(buffer, tok_len, "CP874", NULL);
+        s = pos[i];
+        PyList_Append(result, tok); 
+        Py_XDECREF(tok_cp874);
+        Py_XDECREF(tok);
+    } 
+    if(s < len) {
+        PyObject *tok_cp874 = PySequence_GetSlice(txt_cp874, s, len);
+        Py_ssize_t tok_len;
+        PyObject *tok;
+        PyString_AsStringAndSize(tok_cp874, &buffer, &tok_len);
+        tok = PyUnicode_Decode(buffer, tok_len, "CP874", NULL);
+        PyList_Append(result, tok);
+        Py_XDECREF(tok_cp874);
+        Py_XDECREF(tok);
+    } 
+    Py_XDECREF(txt_cp874);
+
+    free(pos); 
+    return result;
 }
 
 static PyMethodDef module_methods[] = {
